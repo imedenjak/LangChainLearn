f7f35ded61be04973a806ea8df7b008500e0e4dd
imedenjak <ivan.medenjak@gmail.com>
2026-03-07 11:09:30 +0100

    Dynamic model selection

---
+++ 9_middleware_dynamic_response_main.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
+from langchain.messages import HumanMessage, SystemMessage, AIMessage
+
+load_dotenv()
+
+basic_model = init_chat_model(model="gpt-4o-mini")
+advanced_model = init_chat_model(model="gpt-4.1-mini")
+
+
+@wrap_model_call
+def dynamic_model_selection(request: ModelRequest, handler) -> ModelResponse:
+    message_conunt = len(request.state["messages"])
+
+    if message_conunt > 3:
+        model = advanced_model
+    else:
+        model = basic_model
+
+    request.model = model  # obsolete, do not use
+    # request.override(model=model)
+
+    return handler(request)
+
+
+agent = create_agent(
+    model=basic_model,
+    middleware=[dynamic_model_selection],
+)
+
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is 1+1?"),
+    HumanMessage(content="What is 2+1?"),
+    HumanMessage(content="What is 3+1?"),
+    HumanMessage(content="What is 4+1?"),
+]
+
+response = agent.invoke(
+    {
+        "messages": messages,
+    }
+)
+
+print(response["messages"][-1].content)
+print(response["messages"][-1].response_metadata["model_name"])
