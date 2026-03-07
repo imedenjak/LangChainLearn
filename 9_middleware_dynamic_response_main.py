c2c1c9d5b91e490ddd6439d20321065a44b8acd8
imedenjak <ivan.medenjak@gmail.com>
2026-03-07 11:10:19 +0100

    Remove unused imports

--- 9_middleware_dynamic_response_main.py
+++ 9_middleware_dynamic_response_main.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass
 from dotenv import load_dotenv
 
 from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
 from langchain.agents.middleware import ModelRequest, ModelResponse, wrap_model_call
-from langchain.messages import HumanMessage, SystemMessage, AIMessage
+from langchain.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
